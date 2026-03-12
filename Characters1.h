ba1f48fb3443f5189b7fe24f7f59ba20da94726b
Andrii Shyshkivskyi <andreyschuschkuwskuy@gmail.com>
2026-03-12 10:14:06 +0200

    Add class MainHero

--- Characters1.h
+++ Characters1.h
@@ -1,6 +1,5 @@
-#ifndef CHARACTERS_H
-#define CHARACTERS_H
-
+#ifndef CHARACTERS1_H
+#define CHARACTERS1_H
 #include <string>
 #include <iostream>
 
@@ -14,32 +13,15 @@ private:
 
 public:
     MainHero(string nameInp, int healthInp, int xpInp);
-
     MainHero();
     MainHero(string nameInp);
     MainHero(string nameInp, int healthInp);
 
     ~MainHero();
-
+    int getXP() { return XP; }
     string getName() { return name; }
     int getHealth() { return health; }
     void setHealth(int h) { if(h >= 0) health = h; }
-};
-
-class Villager {
-private:
-    string name;
-public:
-    Villager();
-    ~Villager();
-};
-
-class Enemy {
-private:
-    string type;
-public:
-    Enemy();
-    ~Enemy();
 };
 
 #endif
