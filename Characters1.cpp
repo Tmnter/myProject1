47c0db2e4cec12ffcfe3f19954434c2481ad93b1
Andrii Shyshkivskyi <andreyschuschkuwskuy@gmail.com>
2026-03-12 10:20:20 +0200

    Add cpp and h

--- Characters1.cpp
+++ Characters1.cpp
@@ -1,44 +1,50 @@
 #include "Characters1.h"
+#include "Characters2.h"
+#include "Characters3.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 MainHero::MainHero(string nameInp, int healthInp, int XPInp)
-    : name{nameInp}, health{healthInp}, XP{xpInp}
+    : name{nameInp}, health{healthInp}, XP{XPInp}
 {
-    cout << "MainHero fully initialized: " << name << " (HP: " << health << ", XP: " << XP << ")" << endl;
+    cout << "MainHero with 3 arg created" << endl;
 }
 MainHero::MainHero(string nameInp)
     : MainHero(nameInp, 100, 0)
 {
-    cout << "Delegated: Constructor with 1 argument called" << endl;
+    cout << "Constructor with 1 argument created" << endl;
 }
 MainHero::MainHero(string nameInp, int healthInp)
     : MainHero(nameInp, healthInp, 0)
 {
-    cout << "Delegated: Constructor with 2 arguments called" << endl;
+    cout << "Constructor with 2 arguments created" << endl;
 }
 
 MainHero::MainHero()
     : MainHero("Hero", 100, 0)
 {
-    cout << "Delegated: Default constructor called" << endl;
+    cout << "Default MainHero created" << endl;
 }
 
 MainHero::~MainHero() {
-    cout << "Object MainHero (" << name << ") deleted" << endl;
+    cout << "MainHero deleted" << endl;
 }
 
-Villager::Villager() {
-    cout << "Object Villager created" << endl;
-}
+Villager::Villager(string nameInp, string jobInp)
+    : name{nameInp}, job{jobInp} {
+    cout << "Default villager created" << endl;
+    }
 Villager::~Villager() {
-    cout << "Object Villager deleted" << endl;
+    cout << "Villager deleted" << endl;
 }
 
-Enemy::Enemy() {
-    cout << "Object Enemy created" << endl;
+Enemy::Enemy(string Inptype, int Inpdamage)
+    : type{Inptype}, damage{Inpdamage}
+{
+    cout << "Enemy created: " << name << endl;
 }
+
 Enemy::~Enemy() {
-    cout << "Object Enemy deleted" << endl;
+    cout << "Enemy deleted" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include "Characters.h"
+#include "Characters1.h"
+#include "Characters2.h"
+#include "Characters3.h"
 #include <iostream>
 #include <string>
 
@@ -6,20 +8,12 @@ using namespace std;
 
 int main() {
     cout << "--- Testing MainHero Constructors ---" << endl;
-    MainHero hero1("Andriy");
+    MainHero hero1("Olega", 150);
     cout << "Hero 1 Name: " << hero1.getName() << endl;
-    cout << "-------------------------------------" << endl;
     MainHero hero2("Oleg", 150);
     cout << "Hero 2 Name: " << hero2.getName() << " | HP: " << hero2.getHealth() << endl;
-    cout << "-------------------------------------" << endl;
     MainHero hero3("Master", 999, 100);
-    cout << "Hero 3 Name: " << hero3.getName() << " | XP: " << hero3.getXp() << endl;
-    cout << "-------------------------------------" << endl;
-    MainHero hero4;
-    cout << "Hero 4 (Default): " << hero4.getName() << endl;
-    cout << "-------------------------------------" << endl;
-
-    cout << "\n--- Testing Other Classes ---" << endl;
+    cout << "Hero 3 Name: " << hero3.getName() << " | XP: " << hero3.getXP() << endl;
 
     Villager npc1;
     Enemy boss1;
